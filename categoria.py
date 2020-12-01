3a96240f111fe31f26c5cddc50a822ac2b43c6c6
Pedro Vera <pgvera@espol.edu.ec>
2020-11-30 20:44:05 -0500

    No me reconoce la base modificada

--- categoria.py
+++ categoria.py
@@ -12,24 +12,25 @@ def mapper(record):
 
 
 def reducer(key, list_of_values):
- total = 0 
- for t in list_of_values:
+    total = 0 
+    for t in list_of_values:
      #No que procede :v  Me sale este error 'int' object has no attribute 'strip'
      #Le hice algunas pruebas y me bota algunos errores jajaja hay que seguir probando ejemplo al momento de tomar el t parece que lo entiende como un int asi que está feo no ves que es una cadena
-  nocom= t.replace(',','')
-  print(nocom)
-  num= nocom[:-1]
-  print (num)
-  
-  total += float(num)
- mr.emit((key, total))
+        noplus = str(t).replace("+",'')
+        nocom = str(noplus).replace(",", '')
+        numero = float(nocom)
+        print(numero)    
+        #Podrias probar con tu base? porque me sale este error ValueError: could not convert string to float: 'Free' aunque ya hice la migracion de nuevo    
+        total += numero
+
+    mr.emit((key, total))
 
 
 if __name__ == '__main__':
     # Conexión a MongoDB
- cliente = pymongo.MongoClient("mongodb://localhost:27017/")
- base = cliente["googlePlay"]
- coleccion = base["googlecsv"]
- query = {"_id": 0, "Category": 1, "Installs": 1, "Type":1}
- resultado = coleccion.find({}, query)
- mr.execute(resultado, mapper, reducer)
+    cliente = pymongo.MongoClient("mongodb://localhost:27017/")
+    base = cliente["googlePlay"]
+    coleccion = base["googlecsv"]
+    query = {"_id": 0, "Category": 1, "Installs": 1, "Type":1}
+    resultado = coleccion.find({}, query)
+    mr.execute(resultado, mapper, reducer)
